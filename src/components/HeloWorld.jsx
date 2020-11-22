776261add212f908b1c59c8a60d5f3b4b55229e1
Paola <lpaocastrillon@gmail.com>
2020-11-21 19:55:19 -0500

    PlatziVideo-ESLint

---
+++ src/components/HeloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HeloWorld = () => (
+    <h1>Hello World 💜</h1>
+);
+
+
+export default HeloWorld;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HeloWorld from './components/HeloWorld';
+
+
+
+ReactDOM.render(<HeloWorld/>, document.getElementById('app'));
